f276f070b2c3fa3089634cc6a9343c8ab7e89ba8
zhenggzh3 <zhenggzh3@163.com>
2019-08-03 23:47:36 +0800

    update entity account(toString) in main chapter1

--- src/main/java/com/example/chapter1/entity/account.java
+++ src/main/java/com/example/chapter1/entity/account.java
@@ -4,6 +4,10 @@ public class account {
     private String username;
     private String password;
 
+    public String toString() {
+        return username + password;
+    }
+
     public String getUsername() {
         return username;
     }
